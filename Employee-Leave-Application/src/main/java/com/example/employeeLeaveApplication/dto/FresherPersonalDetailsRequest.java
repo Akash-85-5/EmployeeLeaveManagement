36b85e3bfd31af25397685d03b44cd40e809c902
Akash-85-5 <vakash302005@gmail.com>
2026-03-19 13:14:56 +0530

    fullname bug fully cleared

--- Employee-Leave-Application/src/main/java/com/example/employeeLeaveApplication/dto/FresherPersonalDetailsRequest.java
+++ Employee-Leave-Application/src/main/java/com/example/employeeLeaveApplication/dto/FresherPersonalDetailsRequest.java
@@ -20,7 +20,7 @@ import java.time.LocalDate;
 public class FresherPersonalDetailsRequest {
 
     // ── NEW name fields ───────────────────────────────────────────
-    private String fullName;
+    private String firstName;
     private String lastName;
     private String surName;
 
@@ -59,8 +59,14 @@ public class FresherPersonalDetailsRequest {
 
     // Getters & Setters
 
-    public String getFullName() { return fullName; }
-    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
     public String getLastName() { return lastName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
