6c9a0ab07afebe4be59e0bbee74dde8678848a49
Akash-85-5 <vakash302005@gmail.com>
2026-04-03 10:28:21 +0530

    Leave Allocation After Personal Detail Fill

--- Employee-Leave-Application/src/main/java/com/example/employeeLeaveApplication/feature/employee/dto/ProfileResponse.java
+++ Employee-Leave-Application/src/main/java/com/example/employeeLeaveApplication/feature/employee/dto/ProfileResponse.java
@@ -93,7 +93,6 @@ public class ProfileResponse {
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
