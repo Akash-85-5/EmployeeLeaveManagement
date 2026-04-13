7047a8470344312916ffdd789e466e788333bf11
Akash-85-5 <vakash302005@gmail.com>
2026-04-13 13:05:40 +0530

    feat : Update employee

--- src/main/java/com/emp_management/feature/employee/repository/EmployeeOnboardingRepository.java
+++ src/main/java/com/emp_management/feature/employee/repository/EmployeeOnboardingRepository.java
@@ -1,5 +1,6 @@
 package com.emp_management.feature.employee.repository;
 
+import com.emp_management.feature.employee.entity.Employee;
 import com.emp_management.feature.employee.entity.EmployeeOnboarding;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -7,4 +8,5 @@ import java.util.Optional;
 
 public interface EmployeeOnboardingRepository extends JpaRepository<EmployeeOnboarding,Long> {
     Optional<EmployeeOnboarding> findByEmployee_EmpId(String empId);
+    Optional<EmployeeOnboarding> findByEmployee (Employee employee);
 }
